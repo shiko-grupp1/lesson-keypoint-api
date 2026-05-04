d8e89bd50e8005ddfeafec6336243823bbdcd160
osmanjeylani <osmanjeylani00@gmail.com>
2026-05-04 12:20:23 +0200

    set up project for key points and created folders, clean repo and add gitignore

---
+++ LessonService/Domain/Entities/Lesson.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities;
+
+public class Lesson
+{
+	public Guid Id { get; set; }
+	public string Title { get; private set; }
+
+	private readonly List<string> _keyPoints = new();
+	public IReadOnlyCollection<string> KeyPoints => _keyPoints;
+
+	private Lesson() { } 
+
+	public Lesson(string title)
+	{
+		Id = Guid.NewGuid();
+		Title = title;
+	}
+
+	public void AddKeyPoint(string keyPoint)
+	{
+		_keyPoints.Add(keyPoint);
+	}
+}
