1d561a460dcffbaa87a3ba75568b93e7425fedd8
osmanjeylani <osmanjeylani00@gmail.com>
2026-05-07 11:54:39 +0200

    add swagger

--- LessonService/Lesson.API/Program.cs
+++ LessonService/Lesson.API/Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddScoped<LessonService>();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
